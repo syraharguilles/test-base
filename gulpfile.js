2f2e52aeeb3d0e4192b002977e3ae3782bff88cd
Syrah Arguilles <syrah.mae.arguilles@newparts.com>
2025-04-08 17:07:39 +0800

    TEST finalize the code

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,43 @@
 // http://justinmccandless.com/post/a-tutorial-for-getting-started-with-gulp
 
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // ✅ This sets the compiler
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 const cache = require('gulp-cache');
-//New
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename'); // ✅ Add this to avoid errors
 
 // Development Tasks
 // -----------------
 
-
-gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in main.css.source/styles and children dirs
+function buildCoreDev() {
+  return gulp.src('scss/core.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError)) // Passes it through gulp-sass and deal with errors
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(sourcemaps.write('../maps'))
-    .pipe(gulp.dest('css/')) // Outputs it in the css folder
+    .pipe(rename('core.css'))
+    .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream());
-})
+}
 
-gulp.task('sass:production', function() {
-  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in main.css.source/styles and children dirs
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Passes it through a gulp-sass
+function buildVendorDev() {
+  return gulp.src('scss/vendor.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(autoprefixer({ cascade: false }))
-    .pipe(gulpIf('*.css', cssnano())) // Minify css
-    .pipe(gulp.dest('dist/')); // Outputs it in the css folder
-})
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(rename('vendor.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('sass', gulp.parallel(buildCoreDev, buildVendorDev));
 
 // Watch SCSS + HTML and reload
 gulp.task('serve', function () {
@@ -53,28 +58,49 @@ gulp.task('serve', function () {
 // ------------------
 
 // Optimizing Images
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src('../../images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
     .pipe(cache(imagemin({
       interlaced: true,
     })))
-    .pipe(gulp.dest('../../images'))
+    .pipe(gulp.dest('../../images'));
 });
-
-// Build Sequences
-// ---------------
-
-gulp.task('clear', function (done) {
-  return cache.clearAll(done);
-})
-
 
 // Clean dist folder
 gulp.task('clean', function () {
   return del(['dist']);
 });
 
+// Clear cache
+gulp.task('clear', function (done) {
+  return cache.clearAll(done);
+});
+
+// Production Tasks
+// ----------------
+
+function buildCoreProd() {
+  return gulp.src('scss/core.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cssnano())
+    .pipe(rename('core.css'))
+    .pipe(gulp.dest('dist/'));
+}
+
+function buildVendorProd() {
+  return gulp.src('scss/vendor.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cssnano())
+    .pipe(rename('vendor.css'))
+    .pipe(gulp.dest('dist/'));
+}
+
+gulp.task('sass:production', gulp.parallel(buildCoreProd, buildVendorProd));
+
+// Build Sequences
+// ---------------
 
 // Dev: compile sass and watch
 gulp.task('default', gulp.series('sass', 'serve'));
